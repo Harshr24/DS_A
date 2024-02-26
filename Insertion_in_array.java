2732506efcbbc72411c8b782e8ea1fe9a0d87e29
Harsh Rai <137977796+Harshr24@users.noreply.github.com>
2024-02-26 14:30:22 +0530

    Update Insertion_in_array.java

--- Insertion_in_array.java
+++ Insertion_in_array.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public class insert_arr {
+public class Insertion_in_array {
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter array size: ");
     int s = sc.nextInt();
-    int[] a = new int[s];
+    int[] a = new int[s + 1]; // Array size should be one more than user-provided size
     System.out.println("Enter array elements: ");
     for (int i = 0; i < s; i++) {
       a[i] = sc.nextInt();
@@ -16,13 +16,13 @@ public class insert_arr {
     int index = sc.nextInt();
     
     if (index < 0 || index > s) {    
-      System.out.println("Invalid index. Index should be between 0 and " + (s - 1));
+      System.out.println("Invalid index. Index should be between 0 and " + s);
       return;
     }
     
     // Shift elements to the right of the insertion index one position to the right
     for (int i = s - 1; i >= index; i--) {
-      a[i + 1] = a[i];
+      a[i + 1] = a[i]; // Shift elements to the right starting from the last element
     }
     
     // Insert the new value at the insertion index
